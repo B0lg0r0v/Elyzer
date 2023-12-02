963f7f305335f028a62d77152bd5a09e27b5e1da
Bolgorov <115954804+B0lg0r0v@users.noreply.github.com>
2023-12-02 01:59:04 +0100

    /

--- phishalyzer.py
+++ phishalyzer.py
@@ -4,25 +4,56 @@ import re
 from colorama import init as colorama_init
 from colorama import Fore
 from colorama import Style
-
-colorama_init()
+import dns.resolver
+import os
 
 def getFile(eHeader):
-    with open(eHeader, 'r', encoding='UTF-8') as header:
-        lines =  header.read().splitlines()
+    try:
+        with open(eHeader, 'r', encoding='UTF-8') as header:
+            lines =  header.read().splitlines()
 
-    return ''.join(lines)
+        return ''.join(lines).lower()
+    except FileNotFoundError:
+        print(f'{Fore.RED}File not found.{Fore.RESET}')
+        sys.exit(1)
+
+def resolveIP(domain):
+    try:
+        resolve4 = dns.resolver.resolve(domain, 'A')
+        resolve6 = dns.resolver.resolve(domain, 'AAAA')
+        if resolve4:
+            for ipv4 in resolve4:
+                return f'({ipv4})'
+        if resolve4 is None and resolve6:
+            for ipv6 in resolve6:
+                return f'({ipv6})'
+    except:
+        return f'{Fore.LIGHTRED_EX}(No IP){Fore.RESET}'
+    
 
 def routing(header):
     
-    receivedMatch = re.findall(r'Received: from ([\w\-.:]+)', header)
+    routing = []
+
+    receivedMatch = re.findall(r'received: from ([\w\-.:]+)', header)
     byMatch = re.findall(r'by ([\w\-.:]+)', header)
-    withMatch = re.findall(r'with ([\w\/]+);', header)
+    withMatch = re.findall(r'with ([\w\-.:]+)', header)
+    ip = re.findall(r'received: from ([\w\-.:]+) [\(\[]([\w\.:]+)[\)\]]', header)
 
-    for hopsCount, (amountReceived, amountBy, amountWith) in enumerate(zip(reversed(receivedMatch), reversed(byMatch), withMatch), start=1):
-        print(f'Hop {hopsCount}: FROM {amountReceived} BY {amountBy} WITH {amountWith}')
+    print(f'\n{Fore.LIGHTBLUE_EX}Relay Routing: {Fore.RESET}')
+    
 
-    return None
+    for hopsCount, (amountReceived, amountBy, amountWith) in enumerate(zip(reversed(receivedMatch), reversed(byMatch), reversed(withMatch)), start=1):
+        print(f'Hop {hopsCount} |↓|: FROM {Fore.GREEN}{amountReceived}{Fore.RESET} {resolveIP(amountReceived)} TO {Fore.GREEN}{amountBy}{Fore.RESET} {resolveIP(amountBy)} WITH {Fore.CYAN}{amountWith}{Fore.RESET}')
+        routing.append(f'Hop {hopsCount} |↓|: FROM {amountReceived} {resolveIP(amountReceived)} TO {amountBy} {resolveIP(amountBy)} WITH {amountWith}')
+
+    return '\n'.join(routing)
+
+
+def envelopeFrom(header):
+    envelopeFromMatch = re.findall(r"[-A-Za-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?", header)
+    for form in enumerate(envelopeFromMatch, start=1):
+        print(f'{form[1]}')
 
 def main():
 
@@ -34,13 +65,15 @@ def main():
     args = parser.parse_args() #initialize the Parser.
 
     if args.file is not None and args.analyze is not None:
-        routing(getFile(args.file))
+        print(f'{Fore.YELLOW}\nE-Mail Header Analyse complete{Fore.RESET}')
+        #routing(getFile(args.file))
+        print(envelopeFrom(getFile(args.file)))
         #print(getFile(args.file))
-
+        #print(resolveIP('PAWP194MB2202.EURP194.PROD.OUTLOOK.COM'.lower()))
     else:
         parser.error('E-Mail Header is required.')
 
-    
 
-
-main()
+if __name__ == '__main__':
+    colorama_init()
+    main()
