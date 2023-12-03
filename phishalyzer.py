5bb249ff2d527ec62c2bd123e6a54aea44e50b6b
Bolgorov <115954804+B0lg0r0v@users.noreply.github.com>
2023-12-03 20:03:13 +0100

    v0.1

--- phishalyzer.py
+++ phishalyzer.py
@@ -10,6 +10,57 @@ from datetime import datetime
 from email.parser import BytesParser
 
 
+def getReceivedFields(eHeader):
+    # credits goes to spcnvdr for helping me with this part of the code. https://github.com/spcnvdr/tracemail/tree/master Copyright 2020 spcnvdr <spcnvdrr@protonmail.com>
+
+    #------------------------get all the "Receveid: from" Fields------------------------# 
+
+    found = False
+    tmp = ''
+    receivedFields =[]
+    finalReceivedFields = []
+    fields = getFields(eHeader)
+
+    with open(eHeader, 'r', encoding='UTF-8') as header:
+        for lines in header:
+            separator = lines.split()
+            
+            if len(separator) != 0 and separator[0] in fields and found:
+                receivedFields.append(tmp)
+                tmp =''
+                if separator[0] != 'Received:':
+                    found = False            
+                else:
+                    tmp += lines
+            elif found:
+                tmp += lines
+            elif 'Received:' in lines.split():
+                found = True
+                tmp += lines
+
+    for x in receivedFields:
+        finalReceivedFields.append(' '.join(x.split()))
+
+    return finalReceivedFields
+
+
+#------------------------get all the fields------------------------#
+
+def getFields(filename):
+    fields = []
+    # First find all the fields present in the email headers
+    with open(filename, "rb") as fp:
+        headers = BytesParser().parse(fp)
+
+    # Add each field to a list
+    for j in headers:
+        fields.append(j + ":")
+
+    return fields
+
+
+#------------------------resolve to IPv4------------------------#
+
 def resolveIP(domain):
     try:
         resolve4 = dns.resolver.resolve(domain, 'A')
@@ -20,59 +71,61 @@ def resolveIP(domain):
     except:
         return f'{Fore.LIGHTRED_EX}(No IP){Fore.RESET}'
     
-
+#------------------------Routing Information & Timestamps------------------------#
 def routing(eHeader):
     
-    try:
-        with open(eHeader, 'r', encoding='UTF-8') as content:
-            tmp =  content.read().splitlines()
-
-    except FileNotFoundError:
-        print(f'{Fore.RED}File not found.{Fore.RESET}')
-        sys.exit(1)
-
-
-    header = ''.join(tmp).lower()
-    routing = []
-
-    receivedMatch = re.findall(r'received: from ([\w\-.:]+)', header)
-    
-    for countReceived in enumerate(reversed(receivedMatch), start=1):
-        byMatch = re.findall(r'by ([\w\-.:]+)', header)
-    
-    withMatch = re.findall(r'with ([\w\-.:]+)', header)
-
+    routing =[]
+    counter= 0 # counter for the hops
 
     print(f'\n{Fore.LIGHTBLUE_EX}Relay Routing: {Fore.RESET}')
     routing.append(f'Relay Routing:\n')
 
-    for hopsCount, (amountReceived, amountBy, amountWith) in enumerate(zip(reversed(receivedMatch), reversed(byMatch), reversed(withMatch)), start=1):
-        print(f'Hop {hopsCount} |↓|: FROM {Fore.GREEN}{amountReceived}{Fore.RESET} {resolveIP(amountReceived)} TO {Fore.GREEN}{amountBy}{Fore.RESET} {resolveIP(amountBy)} WITH {Fore.CYAN}{amountWith}{Fore.RESET}')
-        routing.append(f'Hop {hopsCount} |↓|: FROM {amountReceived} {resolveIP(amountReceived)} TO {amountBy} {resolveIP(amountBy)} WITH {amountWith}')
+    for y in reversed(getReceivedFields(eHeader)):
+        
+        # Regex for the field values
+        receivedMatch = re.findall(r'received: from ([\w\-.:]+)', y, re.IGNORECASE)
+        byMatch = re.findall(r'by ([\w\-.:]+)', y, re.IGNORECASE)
+        withMatch = re.findall(r'with ([\w\-.:]+)', y, re.IGNORECASE)
+
+        # regex for IPv4 and IPv6 in square brackets or parentheses
+        ipv4 = re.findall(r'[\[\(](\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})[\]\)]', y, re.IGNORECASE)
+        ipv6 = re.findall(r'[\[\(]([A-Fa-f0-9:]+)[\]\)]', y, re.IGNORECASE)
+
+        counter += 1 
+        if len(receivedMatch) != 0:
+            print(f'Hop {counter} |↓|: FROM {Fore.GREEN}{receivedMatch[0].lower()}{Fore.RESET} TO {Fore.GREEN}{byMatch[0].lower()}{Fore.RESET} WITH {Fore.CYAN}{withMatch[0].lower()}{Fore.RESET}')
+            routing.append(f'Hop {counter} |↓|: FROM {receivedMatch[0].lower()} TO {byMatch[0].lower()} WITH {withMatch[0].lower()}')
+        else:
+            print('No match found')
       
-
-    dateMatch1 = re.findall(r'\S{3},[ ]{0,4} \d{1,2} \S{3} \d{1,4} \d{2}:\d{2}:\d{2} [+-]\d{4}', header)
-    dateMatch2 = re.findall(r'\S{3}, \d{2} \S{3} \d{1,4} \d{2}:\d{2}:\d{2}.\d{0,10} [+-]\d{4}', header)
-    dateMatch3 = re.findall(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{0,10} [+-]\d{4}', header)
-
     print(f'\n{Fore.LIGHTBLUE_EX}Timestamps between Hops: {Fore.RESET}')
     routing.append(f'\nTimestamps between Hops:')
 
-    if dateMatch1 is not None:
-        for counter, date in enumerate(reversed(dateMatch1[:hopsCount]), start=1):
-            print(f'Hop {counter}: {Fore.GREEN}{date}{Fore.RESET}')
-            routing.append(f'Hop {counter}: {date}')
-    
-    elif dateMatch2 is not None:
-        for counter, date in enumerate(reversed(dateMatch2[:hopsCount]), start=1):
-            print(f'Hop {counter}: {Fore.GREEN}{date}{Fore.RESET}')
-            routing.append(f'Hop {counter}: {date}')
-    
-    elif dateMatch3 is not None:
-        for counter, date in enumerate(reversed(dateMatch3[:hopsCount]), start=1):
-            print(f'Hop {counter}: {Fore.GREEN}{date}{Fore.RESET}')
-            routing.append(f'Hop {counter}: {date}')
-            
+    dateCounter = 1 #separate counter for the hops in the timestamps
+    #previousDate = None
+
+    for x in reversed(getReceivedFields(eHeader)):
+        dateMatch1 = re.findall(r'\S{3},[ ]{0,4} \d{1,2} \S{3} \d{1,4} \d{2}:\d{2}:\d{2} [+-]\d{4}', x ,re.IGNORECASE)
+        dateMatch2 = re.findall(r'\S{3}, \d{2} \S{3} \d{1,4} \d{2}:\d{2}:\d{2}.\d{0,10} [+-]\d{4}', x, re.IGNORECASE)
+        dateMatch3 = re.findall(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{0,10} [+-]\d{4}', x, re.IGNORECASE)
+
+        if dateMatch1 is not None:
+            for date in reversed(dateMatch1):
+                print(f'Hop {dateCounter}: {Fore.GREEN}{date}{Fore.RESET}')
+                routing.append(f'Hop {dateCounter}: {date}')
+                dateCounter += 1
+        
+        elif dateMatch2 is not None:
+            for date in reversed(dateMatch2):
+                print(f'Hop {dateCounter}: {Fore.GREEN}{date}{Fore.RESET}')
+                routing.append(f'Hop {dateCounter}: {date}')
+                dateCounter += 1
+
+        elif dateMatch3 is not None:
+            for date in reversed(dateMatch3):
+                print(f'Hop {dateCounter}: {Fore.GREEN}{date}{Fore.RESET}')
+                routing.append(f'Hop {dateCounter}: {date}')
+                dateCounter += 1
 
 
     return '\n'.join(routing)
@@ -155,7 +208,7 @@ def envelope(eheader):
         print(f'{Fore.RED}File not found.{Fore.RESET}')
         sys.exit(1)
 
-    print(f'\n{Fore.LIGHTBLUE_EX}Envelope Information: {Fore.RESET}')
+    print(f'\n{Fore.LIGHTBLUE_EX}Interesting Headers: {Fore.RESET}')
 
 
     if content['X-ORIG-EnvelopeFrom'] is not None:
@@ -214,7 +267,7 @@ def envelope(eheader):
     else:
         print(f'X-MS-Exchange-Organization-Network-Message-Id: {Fore.LIGHTRED_EX}No X-MS-Exchange-Organization-Network-Message-Id{Fore.RESET}')
     
-
+    print(f'{Fore.CYAN}\n<-------------------------------------------------->\n{Fore.RESET}')
 
 def spoofing(eheader):
     try:
@@ -225,51 +278,136 @@ def spoofing(eheader):
         sys.exit(1)
 
     print(f'\n{Fore.LIGHTBLUE_EX}Phishing Check: {Fore.RESET}')
-    
+
+    #------------------------Regex and Field definitions------------------------#
+
+    x = next(iter(reversed(getReceivedFields(eheader))), None)
+    ipv4 = re.findall(r'[\[\(](\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})[\]\)]', x, re.IGNORECASE)
     fromMatch = re.search(r'<(.*)>', content['from'])
+
+    #------------------------check for spoofing------------------------#
+    mx = []
+    aRecordsOfMx = []
+
+    # Getting the Domain Name from the "From" Field
+    fromEmailDomain = fromMatch.group(1).split('@')[1]
+    # Getting the MX Records from the Domain Name
+    getMx = dns.resolver.resolve(fromEmailDomain, 'MX')
+    for servers in getMx:
+        mx.append(servers.exchange.to_text().lower())
+
+    # Resolving the A Records from the MX Records
+    for servers in mx:
+        aRecordsOfMx.append(resolveIP(servers))
+
+    
+    
+    print(f'\n{Fore.LIGHTMAGENTA_EX}Checking for SMTP Mismatch...{Fore.RESET}')
+    #print(aRecords)
+    if ipv4:
+        if ipv4[0] in aRecordsOfMx:
+            print(f'{Fore.LIGHTGREEN_EX}No Mismatch detected.{Fore.RESET}')
+        else:
+            print(f'{Fore.LIGHTRED_EX}POTENTIAL SPOOFING DETECTED. INCORRECT SMTP SERVER DETECTED. SENDER SMTP IS "{fromEmailDomain} [{"".join(ipv4[0])}]" AND SHOULD BE "{fromEmailDomain} [{"".join(aRecordsOfMx)}]" <- (CURRENT MX RECORD FOR THIS DOMAIN) {Fore.RESET}')
+
+    else:
+        print(f'{Fore.LIGHTYELLOW_EX}Could not detect SMTP Server. Manual reviewing required.{Fore.RESET}')
+
+    print(f'\n{Fore.LIGHTMAGENTA_EX}Checking for Field Mismatches...{Fore.RESET}')
+
+    
     
     if fromMatch.group(1) is not None and content['reply-to'] is not None:
         if content['from'] != content['reply-to']:
             print(f'{Fore.LIGHTYELLOW_EX}Suspicous activity detected: "FROM" Field ({fromMatch.group(1)}) NOT EQUAL "REPLY-TO" Field ({content["reply-to"]}){Fore.RESET}')
         else:
-            pass
+            print(f'{Fore.LIGHTGREEN_EX}No "FROM - REPLY-TO" Mismatch detected.{Fore.RESET}')
 
     if fromMatch.group(1) is not None and content['return-path'] is not None:
         if fromMatch.group(1) != content['return-path']:
             print(f'{Fore.LIGHTYELLOW_EX}Suspicous activity detected: "FROM" Field ({fromMatch.group(1)}) NOT EQUAL "RETURN-PATH" Field ({content["return-path"]}){Fore.RESET}')
         else:
-            pass
+            print(f'{Fore.LIGHTGREEN_EX}No "FROM - RETURN-PATH" Mismatch detected.{Fore.RESET}')
        
+    #------------------------Check with VirusTotal------------------------#
+
+    print(f'\n{Fore.LIGHTMAGENTA_EX}Checking with VirusTotal...{Fore.RESET}')
+
+    if ipv4:
+        # If you got an VT API Key, you can use it here. It will generate a report for the IP Address. Uncomment the line under this comment and replace <Your API KEY> with your API Key.
+        #os.system(f'curl -s -X GET --header "x-apikey: <Your API KEY>" "https://www.virustotal.com/api/v3/ip_addresses/{ipv4[0]}" > vt.json')
+        print(f'{Fore.LIGHTYELLOW_EX}Note: You can use your own VirusTotal API Key to generate a report for the IP Address. Check the Source Code.{Fore.RESET}\n')
+        print(f'Detections: {Fore.LIGHTGREEN_EX}https://www.virustotal.com/gui/ip-address/{ipv4[0]}/detection{Fore.RESET}')
+        print(f'Relations: {Fore.LIGHTGREEN_EX}https://www.virustotal.com/gui/ip-address/{ipv4[0]}/relations{Fore.RESET}')
+        print(f'Graph: {Fore.LIGHTGREEN_EX}https://www.virustotal.com/gui/ip-address/{ipv4[0]}/graph{Fore.RESET}')
+        print(f'Network Traffic: {Fore.LIGHTGREEN_EX}https://www.virustotal.com/gui/ip-address/{ipv4[0]}/network-traffic{Fore.RESET}')
+        print(f'WHOIS: {Fore.LIGHTGREEN_EX}https://www.virustotal.com/gui/ip-address/{ipv4[0]}/whois{Fore.RESET}')
+        print(f'Comments: {Fore.LIGHTGREEN_EX}https://www.virustotal.com/gui/ip-address/{ipv4[0]}/comments{Fore.RESET}')
+        print(f'Votes: {Fore.LIGHTGREEN_EX}https://www.virustotal.com/gui/ip-address/{ipv4[0]}/votes{Fore.RESET}')
+
+    else:
+        print(f'{Fore.LIGHTYELLOW_EX}Could not detect SMTP Server. Manual reviewing required.{Fore.RESET}')
+
+    #------------------------Check with AbuseIPDB------------------------#
+
+    print(f'\n{Fore.LIGHTMAGENTA_EX}Checking with AbuseIPDB...{Fore.RESET}')
+
+    if ipv4:
+        # If you got an AbuseIPDB API Key, you can use it here. It will generate a report for the IP Address. Uncomment the line under this comment and replace <Your API KEY> with your API Key.
+        #os.system(f'curl -s -X GET --header "Key: <Your API KEY>" "https://api.abuseipdb.com/api/v2/check?ipAddress={ipv4[0]}" > abuseipdb.json')
+        print(f'{Fore.LIGHTYELLOW_EX}Note: You can use your own AbuseIPDB API Key to generate a report for the IP Address. Check the Source Code.{Fore.RESET}\n')
+        print(f'AbuseIPDB: {Fore.LIGHTGREEN_EX}https://www.abuseipdb.com/check/{ipv4[0]}{Fore.RESET}')
+        
+    else:
+        print(f'{Fore.LIGHTYELLOW_EX}Could not detect SMTP Server. Manual reviewing required.{Fore.RESET}')
+
+    #------------------------Check with IPQualityScore------------------------#
+
+    print(f'\n{Fore.LIGHTMAGENTA_EX}Checking with IPQualityScore...{Fore.RESET}')
+
+    if ipv4:
+        # If you got an IPQualityScore API Key, you can use it here. It will generate a report for the IP Address. Uncomment the line under this comment and replace <Your API KEY> with your API Key.
+        #os.system(f'curl -s -X GET --header "Key: <Your API KEY>" "https://www.ipqualityscore.com/api/json/ip/<Your API KEY>/{ipv4[0]}" > ipqualityscore.json')
+        print(f'{Fore.LIGHTYELLOW_EX}Note: You can use your own IPQualityScore API Key to generate a report for the IP Address. Check the Source Code.{Fore.RESET}\n')
+        print(f'IPQualityScore: {Fore.LIGHTGREEN_EX}https://www.ipqualityscore.com/free-ip-lookup-proxy-vpn-test/lookup/{ipv4[0]}{Fore.RESET}')
+
+    else:
+        print(f'{Fore.LIGHTYELLOW_EX}Could not detect SMTP Server. Manual reviewing required.{Fore.RESET}')
 
     
 
-def main():
+if __name__ == '__main__':
+    colorama_init()
+
+    print("\n")
+    print(r"""
+          
+   ____ ____  __ ____   ____ ___ 
+  / __// /\ \/ //_  /  / __// _ \
+ / _/ / /__\  /  / /_ / _/ / , _/
+/___//____//_/  /___//___//_/|_| 
+                                  
+
+    Author: B0lg0r0v
+    https://root.security
+    """)
+    print("\n")
+
 
     parser = ArgumentParser() #Create the Parser.
-
     parser.add_argument('-f', '--file')
     parser.add_argument('-a', '--analyze', action='store_true')
 
     args = parser.parse_args() #initialize the Parser.
 
     if args.file is not None and args.analyze is not None:
-        print(f'{Fore.YELLOW}\nE-Mail Header Analyse complete{Fore.RESET}')
-        
-        #geeneralInformation(args.file)
+        print(f'{Fore.YELLOW}E-Mail Header Analyse complete{Fore.RESET}')
+
+        geeneralInformation(args.file)
         routing(args.file)
-        #securityInformations(args.file)
-        #envelope(args.file)
+        securityInformations(args.file)
+        envelope(args.file)
         spoofing(args.file)
-        #print(getFile(args.file))
-        #print(resolveIP('mail-oa1-f45.google.com'.lower()))
-        #print(extract_date(getFile(args.file)))
+
     else:
         parser.error('E-Mail Header is required.')
-
-
-
-if __name__ == '__main__':
-    colorama_init()
-
-    
-    main()
